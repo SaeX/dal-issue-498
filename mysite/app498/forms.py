2bd3886f81750d8695ae98bc5f72b35252a3257a
F <f@f.f>
2016-09-21 23:16:34 +0200

    Add requirements.txt Remove dependency on bootstrap/datetimepicker/django-crispy-forms

--- mysite/app498/forms.py
+++ mysite/app498/forms.py
@@ -6,20 +6,8 @@ al.autodiscover()
 
 from .models import History
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
 
 class CreateHistoryForm(al.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super(CreateHistoryForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-4'
-        self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Save'))
 
     class Meta:
         model = History
